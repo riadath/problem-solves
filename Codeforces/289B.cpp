bb432e3ae398b090a1b1484a3d9489c167bc621e
Reyadath Ullah Akib <akib152012@gmail.com>
2021-03-07 19:37:42 +0600

    hello

--- Codeforces/289B.cpp
+++ Codeforces/289B.cpp
@@ -1,10 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 #ifdef IHAVETOPEE
 #include "debug.h"
 #else
 #define debug(args...)
 #endif
+
+#pragma GCC target("avx2")
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
 
 long long oll = 0;
 #define ll long long
@@ -37,54 +44,37 @@ bool comp(const pii &a, const pii &b)
 {
     return a.fi < b.fi;
 }
-
+int mat[109][109];
 int main()
 {
     FIO;
     // freopen("in.txt", "r", stdin);
     // freopen("out.txt", "w", stdout);
-    int T;
-    cin >> T;
-    while (T--)
-    {
-        ll n, g, b, gd, bd, ans = 0;
-        cin >> n >> g >> b;
-        if (g >= n)
-        {
-            cout << n << nl;
-            continue;
-        }
-        gd = n / 2 + (n % 2 != 0);
-        bd = n / 2;
-        if (g >= gd)
-        {
-            ans += g;
-            if (b >= n - g)
-                ans += n - g;
-            else
-            {
-                ans += b;
-                ans += max(oll, n - g - b);
+    int n,m,d;
+    cin>>n>>m>>d;
+    vector<int>arr;
+    fl(i,0,n)fl(j,0,m)cin>>mat[i][j],arr.pb(mat[i][j]);
+    sort(all(arr));
+    int len = (int)arr.size();
+    int mn_move = 1e8;
+    for(int k = len/4;k <= (3 * len)/4;k++){
+        int to_comp = arr[k],temp_move = 0;
+        fl(i,0,n){
+            fl(j,0,m){
+                int diff = abs(to_comp - mat[i][j]);
+                if(diff % d != 0){
+                    temp_move = -1;
+                    break;
+                }else{
+                    temp_move += diff/d;
+                }
             }
-            cout << ans << nl;
-            continue;
+            if(temp_move == -1)break;
         }
-        if (g < b)
-        {
-            ll gcy = gd / g; // bcy = bd / b;
-            ans += gcy * (g + b) + max(oll, n - gcy * (g + b));
-            ans += max(oll, (gd - max(oll, n - gcy * (g + b))) - gcy * g);
-            if (g * gcy >= gd)
-            {
-                ans -= b;
-                ans += max(oll, (bd - (gcy - 1) * b));
-            }
-            cout << ans << nl;
-        }
-        else
-        {
-            cout << n << nl;
-        }
+        if(temp_move != -1)
+            mn_move = min(mn_move,temp_move);
     }
+    if(mn_move == 1e8)cout<<-1<<nl;
+    else cout<<mn_move<<nl;
     return 0;
 }
